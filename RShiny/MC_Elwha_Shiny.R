4f5d4d56cdaa6ff4f50d20dc5f34f67373d422d2
Malia-T <mookiethompson17@gmail.com>
2024-02-10 13:01:57 -0800

    Added R Shiny code; map of Elwha with option to choose site and view location

---
+++ RShiny/MC_Elwha_Shiny.R
@@ -0,0 +1,60 @@
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(maps)
+library(sf)
+library(readxl)
+#install.packages("shiny")
+#install.packages("shinythemes")
+library(shiny)
+
+# load shapefile
+elwha_st <- st_read('geo/elwha_streams.shp')
+daily_avg_temps <- read.csv('data/daily-avg-tmp.csv')
+site.names <- read_excel("data/Temperature Site Names.xlsx", "Master")
+
+unique(site.names$Temp_Alias)
+
+# drop Z/M coords
+elwha_shp <- st_zm(elwha_st)
+
+# convert to dataframe
+elwha_df <- st_as_sf(elwha_shp)
+
+# plot elwha
+elwha_map <- ggplot() +
+  geom_sf(data=elwha_df) +
+  labs(title="Elwha River")
+
+ui <- fluidPage(
+  titlePanel("Demo R Shiny Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      # no functionality yet...
+      selectInput("Site", label="Select Site", choices=unique(site.names$Temp_Alias),
+                  selected="ES01")
+    ),
+    mainPanel(
+      # display the map
+      plotOutput("ggplot", height="700px", width='350px')
+    )
+  )
+)
+
+server <- function(input, output) {
+  # render plot?
+  output$ggplot <- renderPlot({
+    # filter based on selected site
+    filtered <- site.names[site.names$Temp_Alias == input$Site, ]
+    
+    # plot elwha and site points
+    ggplot() +
+      geom_sf(data=elwha_df) +
+      geom_point(data=filtered, aes(x=LONG, y=LAT, color='red', size=3)) +
+      labs(title='Elwha River Sites') +
+      theme(legend.position='none')
+  })
+   
+}
+
+shinyApp(ui = ui, server = server)
