354c72a0f3f9e5c85cbc6fa71f9c07069dfbbc0a
atsai23 <anna.tsai23@gmail.com>
2024-02-28 15:44:25 -0800

    Working Filter Code!

--- RShiny/scatterplot.R
+++ RShiny/scatterplot.R
@@ -7,46 +7,49 @@ library(dplyr)
 # Load data --------------------------------------------------------------------
 
 df <- read.csv("mergetemp_test.csv")
+
+#Make sure date column is formated as date
 df <- mutate(df, Date = as.Date(Date, format= "%Y-%m-%d"))
-site_names <- sort(unique(df$ward5))
+
+#Get site names
+site_names <- sort(unique(df$ward5)) 
+#hard coded- might want to have some flexibility for the name of the label column?
+
 
 # Define UI --------------------------------------------------------------------
 
 ui <- fluidPage(
+  titlePanel("Temperature Time Series by Site"),
   
   sidebarLayout(
-    
-    #Input(s)
     sidebarPanel(
       
-      #Filter by site/group
-      selectInput(
-        inputID = "site",
-        label = "Select Group/Site",
+      #Dropdown for site selection
+      selectInput('site',
+        label = 'Choose a Site',
         choices = site_names,
-        selected = 0,
-        multiple = TRUE)
-      
+        selected = site_names[0],
+        multiple = TRUE
+        )
+      ),
+    
+    #Display graph
+    mainPanel(plotOutput('scatterplot')
       )
-    
-    ),
-    
-  # Output: Show scatterplot
-  mainPanel(
-    plotOutput(outputId = "scatterplot")
     )
   )
 
-
 # Define server ----------------------------------------------------------------
-
-server <- function(input, output, session) {
-  
+server <- function(input, output) {
   output$scatterplot <- renderPlot({
+    #Filter for just the selected sites
     sites_subset <- df %>% filter(ward5 %in% input$site) %>% select(Date:Temp)
-    ggplot(data = sites_subset, aes(x = Date, y = Temp)) + geom_point()
+
+    #Code for plots
+    ggplot(data = sites_subset, aes(x = Date, y = Temp)) + 
+      geom_point(aes(color = factor(ward5))) 
+    #ideally which column has the labels would be from user input
   })
-  
 }
 
 # Create a Shiny app object ----------------------------------------------------
