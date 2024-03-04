c0c150e42e7b12153c267e8e903242396d6b0f3f
atsai23 <anna.tsai23@gmail.com>
2024-03-03 17:14:46 -0800

    New Filter Options?

--- RShiny/scatterplot.R
+++ RShiny/scatterplot.R
@@ -6,15 +6,24 @@ library(dplyr)
 
 # Load data --------------------------------------------------------------------
 
-df <- read.csv("mergetemp_test.csv")
+#Temperature Data
+temp <- read.csv('data/daily-avg-tmp.csv')
+
+#Drop NaNs
+temp <- temp[!(is.na(temp$Temp)),]
 
 #Make sure date column is formated as date
-df <- mutate(df, Date = as.Date(Date, format= "%Y-%m-%d"))
+temp <- mutate(temp, Date = as.Date(Date, format = "%Y-%m-%d"))
 
-#Get site names
-site_names <- sort(unique(df$ward5)) 
+#Get site info
+sites <- read_xlsx('data/Temperature Site Names.xlsx')
+
+#Get info for only sites we have measurements for
+measured_sites <- sites %>% filter(Temp_Alias %in% temp$Site)
+
+#Get list of site names
+site_names <- sort(unique(temp$Site))
 #hard coded- might want to have some flexibility for the name of the label column?
-
 
 # Define UI --------------------------------------------------------------------
 
@@ -23,31 +32,42 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      
       #Dropdown for site selection
-      selectInput('site',
+      selectInput(
+        'site',
         label = 'Choose a Site',
         choices = site_names,
-        selected = site_names[0],
+        selected = site_names[0:5],
         multiple = TRUE
-        )
       ),
+      sliderInput(
+        'range',
+        label = 'River KM',
+        min = 0,
+        max = max(measured_sites$RKM),
+        value = c(0, 25)
+        
+        
+      )
+    ),
     
     #Display graph
-    mainPanel(plotOutput('scatterplot')
-      )
-    )
+    mainPanel(plotOutput('scatterplot'))
   )
+)
 
 # Define server ----------------------------------------------------------------
 server <- function(input, output) {
   output$scatterplot <- renderPlot({
     #Filter for just the selected sites
-    sites_subset <- df %>% filter(ward5 %in% input$site) %>% select(Date:Temp)
-
+    sites_subset <-
+      temp %>% filter(Site %in% input$site) %>%
+      select(Date:Temp)
+    
     #Code for plots
-    ggplot(data = sites_subset, aes(x = Date, y = Temp)) + 
-      geom_point(aes(color = factor(ward5))) 
+    ggplot(data = sites_subset, aes(x = Date, y = Temp)) +
+      geom_point(aes(color = factor(Site))) +
+      labs(color = 'Site')
     #ideally which column has the labels would be from user input
   })
 }
